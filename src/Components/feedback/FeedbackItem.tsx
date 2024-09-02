c8621a1ba4d46bac61688e7493b6d4f9b86e1363
KIM YUEN <kim_man@live.com>
2024-09-02 16:52:33 +1000

    feat: add expand/collapse functionality to FeedbackItem component

--- src/Components/feedback/FeedbackItem.tsx
+++ src/Components/feedback/FeedbackItem.tsx
@@ -1,5 +1,6 @@
 import { TriangleUpIcon } from '@radix-ui/react-icons';
 import { TFeedbackItem } from '../../lib/types';
+import { useState } from 'react';
 
 type FeedbackItemProps = {
   feedbackItem: TFeedbackItem;
@@ -7,8 +8,12 @@ type FeedbackItemProps = {
 export default function FeedbackItem({
   feedbackItem: { badgeLetter, company, text, upvoteCount, daysAgo },
 }: FeedbackItemProps) {
+  const [open, setOpen] = useState(false);
   return (
-    <li className="feedback">
+    <li
+      className={`feedback ${open && 'feedback--expand'}`}
+      onClick={() => setOpen((prev) => !prev)}
+    >
       <button>
         <TriangleUpIcon />
         <span>{upvoteCount}</span>
